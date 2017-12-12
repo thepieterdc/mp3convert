201a2745aca6d8931721215f5439379b08a67f55
Pieter De Clercq <pieterdeclercq@outlook.com>
2017-12-12 21:31:20 +0100

    Not converting mp3 files

--- main.cpp
+++ main.cpp
@@ -16,9 +16,11 @@
 
 std::string basename(const std::string &file);
 
-std::string convertMP3(const std::string &file, uint16_t bitrate);
+void convertMP3(const std::string &file, uint16_t bitrate);
 
 void error(const std::string &msg);
+
+std::string extension(const std::string &file);
 
 std::string fileName(const std::string &file);
 
@@ -26,7 +28,7 @@ std::string basename(const std::string &file) {
     return file.substr(0, file.rfind('.'));
 }
 
-std::string convertMP3(const std::string &file, uint16_t bitrate) {
+void convertMP3(const std::string &file, uint16_t bitrate) {
     std::string ffmpeg(FFMPEG_COMMAND);
     std::string br(std::to_string(bitrate) + "k");
     std::string outfile(basename(file) + ".mp3");
@@ -34,13 +36,16 @@ std::string convertMP3(const std::string &file, uint16_t bitrate) {
             ffmpeg + " -y -loglevel panic -i \"" + file + "\" -ab " + br + " -map_metadata 0 -id3v2_version 3 \"" +
             outfile + "\"");
     std::system(command.c_str());
-    return outfile;
 }
 
 void error(const std::string &msg) {
     std::cerr << "Error: " << msg << std::endl;
     system(PAUSE_CALL);
     exit(0);
+}
+
+std::string extension(const std::string &file) {
+    return file.substr(file.rfind('.') + 1);
 }
 
 std::string fileName(const std::string &file) {
@@ -56,7 +61,7 @@ int main(int argc, char *argv[]) {
 
     std::cout << "------------------------------------------------------------" << std::endl;
 
-    std::cout << "| mp3convert 1.0.0 |" << std::setw(39 - static_cast<uint8_t>(floor(log((argc - 1))) + 1))
+    std::cout << "| mp3convert 1.0.1 |" << std::setw(39 - static_cast<uint8_t>(floor(log((argc - 1))) + 1))
               << "| Amount: "
               << (argc - 1) << "|" << std::endl;
 
@@ -72,14 +77,23 @@ int main(int argc, char *argv[]) {
     for (int i = 1; i < argc; ++i) {
         std::string file(argv[i]);
         std::string fileCanonical(fileName(file).substr(0, 30));
-        std::cout << "| Converting: " << fileCanonical
-                  << std::setw(
-                          static_cast<int>(32 - fileCanonical.length() -
-                                           static_cast<uint8_t>(floor(log((argc - 1))) + 1) + floor(log((i))) +
-                                           1)) << " [" << i << "/" << (argc - 1)
-                  << "]|" << std::endl;
 
-        convertMP3(file, bitrate);
+        if (extension(file) != "mp3") {
+            std::cout << "| Converting: " << fileCanonical;
+        } else {
+            std::cout << "| Skipping:   " << fileCanonical;
+        }
+
+        std::cout
+                << std::setw(
+                        static_cast<int>(32 - fileCanonical.length() -
+                                         static_cast<uint8_t>(floor(log((argc - 1))) + 1) + floor(log((i))) +
+                                         1)) << " [" << i << "/" << (argc - 1)
+                << "]|" << std::endl;
+
+        if (extension(file) != "mp3") {
+            convertMP3(file, bitrate);
+        }
     }
 
     return 0;
